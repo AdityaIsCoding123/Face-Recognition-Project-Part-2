68e5999d18f29c4acb973eee2823b65ecb1b6e60
AdityaIsCoding123 <129186133+AdityaIsCoding123@users.noreply.github.com>
2023-11-10 18:54:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality:90
+})
+
+webcam = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log("ML5 VERSION"= ml5.version);
+
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/SJDXhdLV9/",modelLoaded);
+
+function modelLoaded()
+{
+  console.log("Model is Loaded!");
+}
+
+function check()
+{
+
+
+}
+
+function gotResult()
+{
+
+
+}
